1104b8066c4e4240b6c82847e3d94dc6628e48a2
Andy Wright <atwright147@gmail.com>
2018-01-17 14:36:27 +0000

    hook up to redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import List from './components/List';
+import configureStore from './store/configureStore';
+import * as listActions from './actions/listActions';
 
 import './styles/base.scss'
 
-import List from './components/List';
+const initialState = {
+    listItems: [],
+}
+
+const store = configureStore(initialState);
+
+// initialise
+store.dispatch(listActions.updateListItems(items));
 
 ReactDOM.render(
-    <List />,
+    <Provider store={store}>
+        <List />
+    </Provider>,
     document.getElementById('root')
 );
