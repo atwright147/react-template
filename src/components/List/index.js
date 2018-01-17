fad537c090e11496f8849437a0e1c11e40f7480e
Andy Wright <atwright147@gmail.com>
2018-01-17 20:45:25 +0000

    remove unneeded constructor

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -5,11 +5,6 @@ import { connect } from 'react-redux';
 import List from './List.js';
 
 class ListContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { list: {} };
-    }
-
     render() {
         return <List items={this.props.list} />
     }
