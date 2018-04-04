608b21758456d1e103e04e2850073901ef30f500
Andy Wright <atwright147@gmail.com>
2018-04-04 22:10:00 +0100

    use merge to merge common with the dev build config

--- webpack.dev.babel.js
+++ webpack.dev.babel.js
@@ -1,7 +1,9 @@
+import merge from 'webpack-merge';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
-export default {
+import common from './webpack.common.babel.js';
+
+export default merge(common, {
     devtool: 'inline-source-map',
     devServer: {
         contentBase: 'src',
@@ -10,30 +12,18 @@ export default {
         },
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
                 use: {
                     loader: 'babel-loader'
                 }
-            },
-            {
-                test: /\.(sass|scss)$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader']
-                }),
-            },
+            }
         ]
     },
     plugins: [
-        // new ExtractTextPlugin({
-        //     filename: 'bundle.css',
-        //     disable: false,
-        //     allChunks: true
-        // }),
         new HtmlWebpackPlugin({
             template: 'src/index.html',
         }),
     ]
-}
+});
