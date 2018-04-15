1cd09ee88b9bfb0a09c8569999174fcf162b3b0d
Andy Wright <atwright147@gmail.com>
2018-04-15 20:38:56 +0100

    remove router reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux'
 
 import listItems from './listItemsReducer';
 
 const rootReducer = combineReducers({
-    routerReducer,
     listItems,
 });
 
