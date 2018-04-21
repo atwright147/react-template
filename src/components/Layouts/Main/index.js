f6cf834e26dd041f1f118c9257f93a8906c97f9c
Andy Wright <atwright147@gmail.com>
2018-04-21 20:09:17 +0100

    add a route to demonstrate the route asterisk NoMatch functionality

--- src/components/Layouts/Main/index.js
+++ src/components/Layouts/Main/index.js
@@ -10,7 +10,8 @@ class MainLayout extends Component {
                         <Link to="/">Home</Link><br />
                         <Link to="/user-form">User Form</Link><br />
                         <Link to="/address-form">Address Form</Link><br />
-                        <Link to="/another-view">Another View</Link>
+                        <Link to="/another-view">Another View</Link><br />
+                        <Link to="/does-not-exist">A view that doesn&rsquo;t exist</Link>
                     </nav>
                 </header>
                 <main>
