44610d171533f2fff1d2b7e5faff70c1b1d05f92
Andy Wright <atwright147@gmail.com>
2019-04-23 19:18:31 +0100

    use a more elegant method to globally register sinon and chai

--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { expect } from 'chai';
-import sinon from 'sinon';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -10,8 +8,6 @@ Enzyme.configure({ adapter: new Adapter() });
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
-global.expect = expect;
-global.sinon = sinon;
 global.React = React;
 
 // Fail tests on any warning
